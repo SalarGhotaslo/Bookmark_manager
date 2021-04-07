b255a7bf07926bd9ca36f74f5716c03a648a2ba3
Salar Ghotaslo <Salar_@live.co.uk>
2021-04-07 15:41:30 +0100

    added test for viewing urls on bookmarks page

--- app.rb
+++ app.rb
@@ -9,6 +9,8 @@ class BookmarkManager < Sinatra::Base
       get '/' do
         'Bookmark Manager'
       end
+
+      
     
       run! if app_file == $0
     end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -5,4 +5,10 @@ feature 'Viewing bookmarks' do
       expect(page).to have_content 'Bookmark Manager'
     end
   end
+  scenario 'You are able to see bookmarks on the bookmarks page' do
+    visit '/bookmarks'
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
+    expect(page).to have_content 'http://www.google.com'
+  end
 end
